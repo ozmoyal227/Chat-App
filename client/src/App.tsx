8b3957529226c0268e2604329ee2086ae5c9a0d4
ozmoyal227 <moyal.oz.18@gmail.com>
2021-11-02 21:37:41 +0200

    init vanilla version

--- client/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
-
-function App() {
-  return <div>Hello</div>;
-}
-
-export default App;
--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-
+// app.use(express.static(path.resolve(__dirname, "../client/index.html")));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/index.html"));
+});
 // =============
 // API
 // =============
