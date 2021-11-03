ff68c1bdcaf2da5c353ca531b2bc5e44bbcef088
ozmoyal227 <moyal.oz.18@gmail.com>
2021-11-03 19:26:31 +0200

    socket

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
-const path = require("path");
-const express = require("express");
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server, Socket } = require("socket.io");
+const io = new Server(server);
 
 const PORT = process.env.PORT || 3001;
-const app = express();
+const path = require("path");
+const { Console } = require('console');
 
-// app.use(express.static(path.resolve(__dirname, "../client/index.html")));
+
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/index.html"));
 });
@@ -18,10 +24,20 @@ app.get("/api", (req, res) => {
 // =============
 // Socket IO
 // =============
+io.on('connection', (socket) => {
+  socket.broadcast.emit('chat-message', 'A user connected');
+  
+  socket.on('chat-message', (msg) => {
+    io.emit('chat-message', msg);
+  });
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('chat-message', 'A user disconnected');
+  });
+});
 
 // =============
 // Server
 // =============
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
