ebe0a972893d38c872b5b772bf36afdcf9bf0ed6
Ammo <fakeam26@gmail.com>
2022-01-11 21:38:56 +0200

    update

--- api/chats/chat.route.js
+++ api/chats/chat.route.js
@@ -3,16 +3,10 @@ import chatsController from "./chat.controller.js";
 import { isAuthMiddleware } from "../../middleware/auth.middleware.js";
 const chatRouter = express.Router();
 
-chatRouter
-  /** GET /chats - Get chats page */
-  .get("/", isAuthMiddleware, chatsController.getChats);
+/** GET /chats - Get chats page */
+chatRouter.get("/", isAuthMiddleware, chatsController.getChats);
 
-chatRouter
-  /** POST /chats - Add message to chat */
-  .get("/", isAuthMiddleware, chatsController.getChats);
-
-chatRouter
-  /** POST /chats/:chatId/messages - Add message to chat */
-  .post("/:chatId/messages", chatsController.addMessageToChat);
+/** POST /chats/:chatId/messages - Add message to chat */
+// chatRouter.post("/:chatId/messages", chatsController.addMessageToChat);
 
 export default chatRouter;
