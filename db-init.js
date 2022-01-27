ec84a7908bd0e26074d6b8ea1677997588a54526
ozmoyal227 <moyal.oz.18@gmail.com>
2022-01-27 13:28:51 +0200

    env config

--- db-init.js
+++ db-init.js
@@ -3,19 +3,25 @@ import initRoom from "./models/room.js";
 import initUser from "./models/user.js";
 // import roomsService from "./services/rooms.service.js";
 
-const sequelize = new Sequelize(
-  `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-  {
-    logging: false,
-    ssl: true,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  }
-);
+
+const sequelize = (JSON.stringify(process.env.DB_HOST) === JSON.stringify("localhost")) ?
+ new Sequelize(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    {
+      logging: false,
+    }
+  ) :
+ new Sequelize(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+      {
+        logging: false,
+        ssl: true,
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    );
 
 const db = {
   sequelize: sequelize,
